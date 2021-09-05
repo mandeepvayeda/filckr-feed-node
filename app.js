f053295b61596d1acbe889a5f5a4097b77ff0ba0
mandip <mandeepsupernova@gmail.com>
2021-09-05 20:05:06 +0530

    initial

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const https = require('https');
+const express = require('express')
+const fs = require('fs');
+
+const app = express()
+
+app.listen(3000);
+
+app.set('view engine', 'ejs');
+
+ app.get('/', (req, res) => {
+	  
+	 https.get('https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1', (resp) => {
+	 resp.setEncoding('utf8');
+	 let rawData = '';
+	 resp.on('data', (chunk) => { rawData += chunk; });
+	 console.log(rawData);
+	 resp.on('end', () => {
+		try {
+			const parsedData = JSON.parse(rawData).items; 
+			res.render('index', { parsedData });
+		} catch (e) {
+			console.error(e.message);
+		}
+	 });
+	 
+	});
+});
